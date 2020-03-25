e3c12e5a617984655d4268700fc2babeaf67c4d7
Muko <alice870103@gmail.com>
2020-03-25 13:15:35 +0800

    update schedule main page & detail page of selecting-phase1

--- src/app/schedule/tab1.page.ts
+++ src/app/schedule/tab1.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  // 圖片的位置
+  slides = [
+    {
+      img: 'assets/img/1.jpg',
+      title: '熱門景點',
+      subtitle: '高雄市'
+    },
+    {
+      img: 'assets/img/2.jpg',
+      title: '熱門景點',
+      subtitle: '合歡山'
+    },
+    {
+      img: 'assets/img/3.jpg',
+      title: '哪裡哪裡',
+      subtitle: 'Good'
+    },
+    {
+      img: 'assets/img/4.jpg',
+      title: '熱門景點4',
+      subtitle: ''
+    }
+  ];
+
+  // 讓圖片進行輪播
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true,
+    speed: 2000,
+   };
   constructor() {}
 
 }
