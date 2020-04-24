bd90b4c22567e8c3f515e792b4e6e19b68c761a0
旻昌 <hardco2020@gmail.com>
2020-04-24 21:16:41 +0800

    框架改變
    還存在小bug

--- src/app/schedule/outcome/outcome.page.ts
+++ src/app/schedule/outcome/outcome.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit ,NgZone ,ViewChild, AfterContentInit} from '@angular/core';
-import { ControllerserviceService,Favorites } from '../../controllerservice.service'
+import { ControllerserviceService,Favorites,googleInfor } from '../../controllerservice.service'
 import { ActivatedRoute,Router } from '@angular/router';
 //import { ConsoleReporter } from 'jasmine';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
@@ -13,12 +13,34 @@ declare var google;
   templateUrl: './outcome.page.html',
   styleUrls: ['./outcome.page.scss'],
 })
+
 export class OutcomePage implements OnInit {
    // 透過 url 將 selection 傳遞到此頁面
    favorite : Favorites = {
     place: "測試",
     collection: 1 
    };
+   test : googleInfor = {
+    distance: 1,
+    openingorNot:2,
+    phoneNumber:3,
+    openPeriod:4,
+   };
+   tests : googleInfor = {
+    distance: 1,
+    openingorNot:2,
+    phoneNumber:3,
+    openPeriod:4,
+   };
+   testss : googleInfor = {
+    distance: 1,
+    openingorNot:2,
+    phoneNumber:3,
+    openPeriod:4,
+   };
+   googles: googleInfor[] = [this.test,this.tests,this.testss];
+   
+   
    favorites: Favorites[]; //load進所有現存資料
    data: any;
    map;
@@ -26,7 +48,8 @@ export class OutcomePage implements OnInit {
    openingorNot : any;
    openPeriod : any;
    phoneNumber : any;
-   example ="台北市中正區開封街一段14巷劉山東牛肉麵"; //到時候會改成所有地點的資料
+   example ="高雄小巨蛋"; //到時候會改成所有地點的資料
+   examples = ["鍋呆子鍋燒麵","老紀牛肉麵","高雄市立圖書館左新分館"];
    constructor(private route: ActivatedRoute, private router: Router ,private zone: NgZone,private geolocation: Geolocation , public service : ControllerserviceService, private loadingController: LoadingController,private nav: NavController) { 
      this.route.queryParams.subscribe(param=>{
        if(param && param.special){
@@ -43,7 +66,6 @@ export class OutcomePage implements OnInit {
     this.service.getFavorites().subscribe(res => {
       this.favorites = res; //接受firebase裡所有的欄位
     });
-    
     this.favorite.place =this.example; //將地址存進等等要放進firebase的地址裡 
     // this.service.addFavorite(this.favorite).then(() => { 每次存都會需要先新增欄位，用此處來新增欄位
     // }); 
@@ -107,32 +129,102 @@ export class OutcomePage implements OnInit {
 
 
     
-    
+   
     //篩選出一系列地點後根據使用者的距離 金錢等等要求 做google place進一步二階段篩選
-    this.GoogleAutocomplete.getPlacePredictions({ input: this.example },  //用此地址作為範例
-      (predictions, status) => {
-        this.zone.run(() => {
-          predictions.forEach((prediction) => {  
-              console.log(prediction)
-              let service = new google.maps.places.PlacesService(this.map);  //將算出的prediction id丟進detail來找到進階資訊
-              service.getDetails(
-                {placeId: prediction.place_id},
-                  (results, status) =>{
-                    console.log(results);
-                    console.log(results.opening_hours.open_now);  //知道現在有沒有開 
-                    if(results.opening_hours.open_now==false){
-                      this.openingorNot = "休業中";
-                    }else{
-                      this.openingorNot = "營業中";
+    for(let i=0;i<=this.examples.length;i++){
+      this.GoogleAutocomplete.getPlacePredictions({ input: this.examples[i] },  //用此地址作為範例
+        (predictions, status) => {
+          this.zone.run(() => {
+            predictions.forEach((prediction) => {  
+                let service = new google.maps.places.PlacesService(this.map);  //將算出的prediction id丟進detail來找到進階資訊
+                service.getDetails(
+                  {placeId: prediction.place_id},
+                    (results, status) =>{
+                      console.log(results);
+                      // console.log(results.opening_hours.open_now);  //知道現在有沒有開 
+                      if(results.opening_hours.open_now==false){
+                        this.googles[i].openingorNot= "休業中";
+                  
+                      }else{
+                        this.googles[i].openingorNot = "營業中";
+                      }
+                      console.log(results.opening_hours.weekday_text);
+                      console.log(i);
+                      
+                      this.googles[i].distance = i;
+                      console.log(this.googles[i].distance);
+                      this.googles[i].openPeriod = results.opening_hours.weekday_text;
+                      this.googles[i].phoneNumber = results.formatted_phone_number;
+                      
                     }
-                    this.openPeriod = results.opening_hours.weekday_text;
-                    this.phoneNumber = results.formatted_phone_number;
-                  }
-             );
+               );
+          });
         });
       });
-    });
+      this.geocoder.geocode({ 'address': this.examples[i]},  (results, status)  => { //先找到當地的經緯度 
+        let pos;
+          if (status == google.maps.GeocoderStatus.OK) {
+              pos = {                                         //目標經緯度
+                lat: results[0].geometry.location.lat(),
+                lng: results[0].geometry.location.lng()
+              };
+              this.geolocation.getCurrentPosition().then((resp) => {  //自己的經緯度
+                // resp.coords.latitude
+                // resp.coords.longitude
+               }).catch((error) => {
+                 console.log('Error getting location', error);
+              });
+              let watch = this.geolocation.watchPosition();
+              watch.subscribe((data) => {
+               // 兩者合併算距離
+               //console.log(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude)));
+               if(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude))>=1000){
+  
+                 this.distance = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude))/1000;
+                 
+                 this.distance = Math.round(this.distance);
+                 this.distance = this.distance +"公里";
+                 this.googles[i].distance = this.distance;
+                 
+               }else{
+                this.distance = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude));
+                this.distance = Math.round(this.distance);
+                this.distance = this.distance +"公尺";
+                this.googles[i].distance = this.distance;
+               }
+               // 四捨五入
+              });      
+          }
+      });  
+      console.log(this.distance);
+    }
+  
+    ////////////////////測試
+    // this.GoogleAutocomplete.getPlacePredictions({ input: this.example },  //用此地址作為範例
+    //   (predictions, status) => {
+    //     this.zone.run(() => {
+    //       predictions.forEach((prediction) => {  
+    //           //console.log(prediction)
+    //           let service = new google.maps.places.PlacesService(this.map);  //將算出的prediction id丟進detail來找到進階資訊
+    //           service.getDetails(
+    //             {placeId: prediction.place_id},
+    //               (results, status) =>{
+    //                 //console.log(results);
+    //                 //console.log(results.opening_hours.open_now);  //知道現在有沒有開 
+    //                 if(results.opening_hours.open_now==false){
+    //                   this.openingorNot = "休業中";
+    //                 }else{
+    //                   this.openingorNot = "營業中";
+    //                 }
+    //                 this.openPeriod = results.opening_hours.weekday_text;
+    //                 this.phoneNumber = results.formatted_phone_number;
+    //               }
+    //          );
+    //     });
+    //   });
+    // });
     ////////////找到兩地距離  1.先找到兩個地方的經緯度 再使用function計算出距離
+    
     this.geocoder.geocode({ 'address': this.example},  (results, status)  => { //先找到當地的經緯度 
       let pos;
         if (status == google.maps.GeocoderStatus.OK) {
@@ -149,7 +241,7 @@ export class OutcomePage implements OnInit {
             let watch = this.geolocation.watchPosition();
             watch.subscribe((data) => {
              // 兩者合併算距離
-             console.log(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude)));
+             //console.log(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude)));
              if(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude))>=1000){
 
                this.distance = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(pos.lat, pos.lng), new google.maps.LatLng(data.coords.latitude,data.coords.longitude))/1000;
@@ -181,7 +273,16 @@ export class OutcomePage implements OnInit {
       }
       
     });
+<<<<<<< Updated upstream
     console.log(this.favorite);
   }*/
+=======
+    console.log(this.favorite.collection);
+    console.log(this.googles);
+  }
+  Test(){
+    console.log(this.googles);
+  }
+>>>>>>> Stashed changes
  
 }
