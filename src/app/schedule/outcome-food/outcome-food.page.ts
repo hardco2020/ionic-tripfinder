e29599efa73ffef5461dbc797cc524214c9a6a3f
chuhsiu <zzh32224@gmail.com>
2020-07-06 01:59:51 +0800

    outcome food page

--- src/app/schedule/outcome-food/outcome-food.page.ts
+++ src/app/schedule/outcome-food/outcome-food.page.ts
@@ -43,7 +43,26 @@ export class OutcomeFoodPage implements OnInit {
     alcohol: 'y',
     dessert: 'y',
     favorite: 'n'
-  }];
+  },{ "Aid": 347, 
+    Aname: "覓奇頂級日本料理高雄龍蝦和牛美食餐廳",
+    photo: "CmRaAAAAEuZF2eY7_nJE5QKenLFrmXbPM7CPgBDrZwMJzt9y4GkbEDpaXHO5zRv69t2508OnFLgbw7ToA6d7KUnBoG0nME72ynaQPNI3DWxpODCfxsjM7KnPvcUkJYeFQ2AI-2flEhCuQXjeNSlLL0piGoGAKFbaGhRVD4LHj6iQP3XDdGFrWNp-DYNpGg",
+    GoogleClass: "restaurant",
+    Phone: "07 269 5111",
+    Address: "802台灣高雄市苓雅區成功一路76號",
+    Rate: 4.4,
+    restaurant: 'y',
+    vendor: "n", 
+    fast_food: "n", 
+    vegetarian_food: "y", 
+    chinese: "y", 
+    exotic: "n", 
+    parity: "n",
+    boxed_lunch: "n",
+    baking: "n",
+    seafood: "n",
+    alcohol: "n",
+    dessert: "n"
+}];
 
   constructor(
     private route: ActivatedRoute,
@@ -59,30 +78,37 @@ export class OutcomeFoodPage implements OnInit {
     });
   }
 
-  ngOnInit(): void{
+  ngOnInit() {
 
-    var sql_func = 'SELECT * FROM FoodInfo WHERE sweet = "' + this.data.sweet +
-                    '" AND salty = "' + this.data.salty +
-                    '" AND cheap = "' + this.data.cheap +
-                    '" AND expensive = "' + this.data.expensive +
-                    '" AND buffet = "' + this.data.buffet +
-                    '" AND chinese = "' + this.data.chinese +
-                    '" AND western = "' + this.data.western +
-                    '" AND japanKorean = "' + this.data.japanKorean +
-                    '" AND SoutheastAsian = "' + this.data.SoutheastAsian +
-                    '" AND netbeauty = "' + this.data.netbeauty +
-                    '" AND vendor = "' + this.data.vendor +
-                    '" AND restaurant = "' + this.data.restaurant +
-                    '" AND alcohol = "' + this.data.alcohol +
-                    '" AND favorite = "n"';;
-
-    var sql_text = "SELECT * FROM FoodInfo";// WHERE Aname = '義大遊樂世界聖托里尼山城'
-    
-    this.sqliteDB.getRestaurantsbycondition(sql_text).then(res => {
-      this.alldata = res
-    })
+    this.alldata = [{      //所有的data
+      "Aid": 1,
+      "Aname": "大立百貨空中遊樂園",
+      "photo": "CmRaAAAAIClT_Ynie6i7diws5vPTvz4IK7-cyWX93fkUtbAnI2EkIORzchzhzZARIHaaF6vDQTx78ZEIEjNX55fzXE2v8aSZ2PArtcX8rP2a7JeimjeaerACyg9ftF37z6p0cBnwEhDEW3u4n-x7AeGI2F5ApFAsGhSFoyAOGS2JKfDqLSLgRjabwJKxyQ",
+      "GoogleClass": "amusement_park",
+      "Phone": "07 261 3060",
+      "Address": "801台灣高雄市前金區五福三路59號RF",
+      "Rate": 4.2
+       },
+       {
+       "Aid": 2,
+       "Aname": "澄清湖風景區 兒童樂園",
+       "photo": "CmRaAAAAQNI4jsV8_g86CEU7tWjZQHtUOMN0mu3aL1sKekjMvWyGqCmyiz9miFN1v5WT6yoRh1KOIb_jmVK38p_Vfy60tcx8TI_hMJr9RQNSq-VtahYFp9OvIOGt5CGSssc_fyD0EhBpe0PveufVdBpENptCQqkqGhRd9VvD2vhgAxF_37fgh5eUlYENtw",
+       "GoogleClass": "amusement_park",
+       "Phone": "07 370 0821",
+       "Address": "833台灣高雄市鳥松區",
+       "Rate": 4
+       },
+       {
+         "Aid": 3,
+         "Aname": "鈴鹿賽道樂園",
+         "photo": "CmRaAAAAG8PNms_SXLnkhz00HQX-67phd-iMq8tNWMXrV8Gsv_l_oaBzi04AqrZsN4cbbHiz3NynrvZU0hd0LnuJpFtj3YuH3Rtd9Z6bhN-3kiS63zMjLoPgunF8SNbpUDtjyqlMEhAyqYYRWyTYj5kKDW05xWOpGhTTJNhM9B8xsRScOXNXZI-rdnyaOA",
+         "GoogleClass": "amusement_park",
+         "Phone": "07 796 7766",
+         "Address": "806台灣高雄市前鎮區中安路1-1號",
+         "Rate": 4.3
+       }
+    ]
   }
-  
   async presentLoading() { // 等待Sign
     const loading = await this.loadingController.create({
       message: '添加中',
